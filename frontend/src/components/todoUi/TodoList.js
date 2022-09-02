04894ee6c039ad48956d33ecc3689ebd7295f8f2
rkshristi123 <shristikumari759@gmail.com>
2022-09-02 12:43:08 +0530

    change css

--- frontend/src/components/todoUi/TodoList.js
+++ frontend/src/components/todoUi/TodoList.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import { HStack, VStack,Text, Flex,Checkbox  } from '@chakra-ui/react'
+import { HStack, VStack,Text, Flex,Checkbox,Box ,Label } from '@chakra-ui/react'
 import { DeleteIcon, EditIcon} from '@chakra-ui/icons'
 import Edit from './Edit'
 
-const todo=["work","project"]
+const todo=[
+  {
+    "title":"todo ui",
+    "des":"in todolist i have to add css more"
+  },
+  {
+    "title":"project",
+    "des":"fetch the data  from the backend and show in the component "
+  }
+]
 
 const TodoList = () => {
   return (
     <VStack   >
     {todo.map((el,i)=>(
- <HStack spacing="20px" w="400px" key={i} borderBottom="0.01px solid 	#D3D3D3" >
- <Flex p={3} w="300px" h="50px" justifyContent="space-between" >
- <Text>{el}</Text>
+ <HStack  w="400px" h="auto" key={i} borderBottom="0.01px solid 	#D3D3D3" >
+ <Flex p={3} w="400px"  justifyContent="space-around" >
+  <Box h="auto" >
+   
+  <Checkbox size='sm' colorScheme='green' mt="-4px"  border="grey">{el.title}</Checkbox>
+ <Text>{el.des}</Text>
+  </Box>
 
- <Flex w="10px" >
- <Checkbox size='sm' colorScheme='green' mt="-4px" mr="2" border="grey"></Checkbox>
- <DeleteIcon color="red.500" mr="2" />
+
+ <Flex w="10px"  flexDirection="column" gap={5} ml="auto">
+
+ <DeleteIcon color="red.500"  />
 
  <Edit/>
 
