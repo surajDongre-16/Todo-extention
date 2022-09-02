a08e81d731712c0f1d718bc3407eda84598f72b8
surajDongre-16 <surajdongre8@gmail.com>
2022-09-02 15:03:53 +0530

    work in progress

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -5,56 +5,71 @@ const { getAllUser, signup, login } = require("../controller/user-controller");
 const passport = require("../config/googleOauth");
 const User = require("../model/User");
 const router = express.Router();
+const cors = require("cors");
+
+
 
 router.get("/", getAllUser);
-
+router.use(cors());
 router.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["email", "profile"] })
 );
 
-// router.get(
-//   "/auth/google/callback",
-//   passport.authenticate("google", {
-//     successRedirect:"/",
-//     failureRedirect: "/user/login/failure",
-//     session: false,
-//   }),
-  
-// );
+router.get("/google/success", (req, res) => {
+  if (req.user) {
+    res.status(401).json({
+      success: false,
+      message: "Login successfull",
+      user:req.user
+    });
+  }
+});
+
+router.get("/google/failure", (req, res) => {
+  res.status(401).json({
+    success: false,
+    message: "Failed to login",
+  });
+});
 
 router.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    failureRedirect: "/auth/google/failure",
-    session: false,
+    successRedirect:"/",
+    failureRedirect: "/google/failure",
   }),
-  async function (req, res) {
 
-    try {
-      const existinguser = await User.findOne({ email: req.user.email });
-    //   console.log(existinguser, "exist");
-      const token = jwt.sign(
-        {
-          name: existinguser.name,
-          id: existinguser._id,
-        },
-        "SECRET"
-      );
-      return res
-        .status(200)
-        .json({
-          message: "Login sucessful",
-          token: token,
-          id: existinguser.id,
-        });
-      
-    } catch (err) {
-      return console.log("err");
-    }
-    // res.redirect("/");
-  }
 );
+
+// router.get(
+//   "/auth/google/callback",
+//   passport.authenticate("google", {
+//     failureRedirect: "/auth/google/failure",
+//     session: false,
+//   }),
+//   async function (req, res) {
+//     try {
+//       const existinguser = await User.findOne({ email: req.user.email });
+//       //   console.log(existinguser, "exist");
+//       const token = jwt.sign(
+//         {
+//           name: existinguser.name,
+//           id: existinguser._id,
+//         },
+//         "SECRET"
+//       );
+//       return res.status(200).json({
+//         message: "Login sucessful",
+//         token: token,
+//         id: existinguser.id,
+//       });
+//     } catch (err) {
+//       return console.log("err");
+//     }
+//     // res.redirect("/");
+//   }
+// );
 
 router.post("/signup", signup);
 router.post("/login", login);
--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
 import TodoMain from "../components/todoUi/TodoMain";
-import ActivityList from './ActivityList'
+import ActivityList from "./ActivityList";
 
 const Home = () => {
-	return (
-		<div id="home">
-			<Navbar />
-			<TodoMain/>
-			{/* <ActivityList/> */}
+  const [user, setUser] = useState(null);
 
-		</div>
-			
-	);
+  useEffect(() => {
+    const getUser = () => {
+      fetch("http://localhost:5000/user/google/success", {
+        method: "GET",
+        credentials: "include",
+        headers: {
+        //   Accept: "application/json",
+          "Content-Type": "application/json",
+        //   "Access-Control-Allow-Credentials": true,
+        },
+      })
+        .then((res) => {
+          if (res.status === 200) return res.json();
+          throw new Error("Authentication has been failed");
+        })
+        .then((res) => {
+          setUser(res.user);
+        })
+        .catch((err) => console.log(err));
+    };
+	getUser()
+  }, []);
+
+  console.log(user,"user")
+  return (
+    <div id="home">
+      <Navbar />
+      <TodoMain />
+      {/* <ActivityList/> */}
+    </div>
+  );
 };
 
 export default Home;
