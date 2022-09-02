c63161c74ee7e2732a25b06b23584dbf4bb35f6f
jksingh1504 <boby150499@gmail.com>
2022-09-02 12:10:37 +0530

    conflicts removed

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -33,7 +33,6 @@ import {
 
 import AddTaskModal from "./AddTaskModal";
 
-
 import { AiOutlineSetting } from "react-icons/ai";
 import { FiLogOut } from "react-icons/fi";
 import { MdToday } from "react-icons/md";
@@ -49,90 +48,12 @@ const Navbar = () => {
 	};
 
 	return (
-    <div id="navbar">
-      <div>
-        <div>
-          <MenuIcon onClick={onOpen} />
-          <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
-            <DrawerOverlay />
-
-            <DrawerContent>
-              <DrawerHeader>Align Your Todo</DrawerHeader>
-              <DrawerBody id="drawerChild">
-                <div
-                  onClick={(e) => {
-                    handleClick("today");
-                  }}
-                  className={activeTab == "today" ? "active" : ""}
-                >
-                  <TodayIcon />
-                  <p>Today</p>
-                </div>
-                <div
-                  onClick={(e) => {
-                    handleClick("upcoming");
-                  }}
-                  className={activeTab == "upcoming" ? "active" : ""}
-                >
-                  <CalendarMonthIcon />
-                  <p>Upcoming</p>
-                </div>
-                <div
-                  onClick={(e) => {
-                    handleClick("completed");
-                  }}
-                  className={activeTab == "completed" ? "active" : ""}
-                >
-                  <CheckCircleOutlineIcon />
-                  <p>Completed</p>
-                </div>
-              </DrawerBody>
-            </DrawerContent>
-          </Drawer>
-        </div>
-        <div>
-          <HomeIcon />
-        </div>
-        <div>
-          <SearchIcon />
-          <input type="text" />
-        </div>
-      </div>
-      <div>
-        <div>
-          <AddIcon />
-        </div>
-        <div>
-          <CheckCircleOutlineIcon />
-        </div>
-        <div>
-          <NotificationsIcon />
-        </div>
-        <div>
-          <Menu>
-            <MenuButton
-              as={Button}
-              borderRadius="50%"
-              textAlign="center"
-              height="28px"
-              backgroundColor="white"
-            >
-              jk
-            </MenuButton>
-            <MenuList>
-              <Box p="0 1rem" borderBottom="1px solid #f0f0f0">
-                <Flex h="4rem" alignItems={"center"}>
-                  <Avatar
-                    name="Kola Tioluwani"
-                    src="https://bit.ly/tioluwani-kolawole"
-                  />
-                  <Flex flexDirection={"column"} ml="1rem">
-                    <Text>Suraj</Text>
-                    <Text fontSize={"0.8rem"}>surajdongre8@gmail.com</Text>
-                  </Flex>
-                </Flex>
-              </Box>
-
+		<div id="navbar">
+			<div>
+				<div>
+					<MenuIcon onClick={onOpen} />
+					<Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+						<DrawerOverlay />
 
 						<DrawerContent>
 							<DrawerHeader>Align Your Todo</DrawerHeader>
@@ -178,7 +99,7 @@ const Navbar = () => {
 			</div>
 			<div>
 				<div>
-					<AddTaskModal />
+					<AddTaskModal/>
 				</div>
 				<div>
 					<CheckCircleOutlineIcon />
@@ -186,6 +107,8 @@ const Navbar = () => {
 				<div>
 					<NotificationsIcon />
 				</div>
+				<div></div>
+
 				<div>
 					<Menu>
 						<MenuButton
@@ -198,70 +121,69 @@ const Navbar = () => {
 							jk
 						</MenuButton>
 						<MenuList>
-							<MenuItem>Download</MenuItem>
-							<MenuItem>Create a Copy</MenuItem>
-							<MenuItem>Mark as Draft</MenuItem>
-							<MenuItem>Delete</MenuItem>
-							<MenuItem>Attend a Workshop</MenuItem>
+							<Box p="0 1rem" borderBottom="1px solid #f0f0f0">
+								<Flex h="4rem" alignItems={"center"}>
+									<Avatar
+										name="Kola Tioluwani"
+										src="https://bit.ly/tioluwani-kolawole"
+									/>
+									<Flex flexDirection={"column"} ml="1rem">
+										<Text>Suraj</Text>
+										<Text fontSize={"0.8rem"}>surajdongre8@gmail.com</Text>
+									</Flex>
+								</Flex>
+							</Box>
+							<MenuItem
+								mt="1rem"
+								onClick={(e) => {
+									handleClick("today");
+								}}
+								className={activeTab === "today" ? "active" : ""}
+							>
+								<MdToday fontSize={"1.4rem"} color="purple" />
+								<Text ml="1rem">Today</Text>
+							</MenuItem>
+							<MenuItem
+								onClick={(e) => {
+									handleClick("upcoming");
+								}}
+								className={activeTab === "upcoming" ? "active" : ""}
+							>
+								<FaRegCalendarAlt fontSize={"1.4rem"} color="brown" />
+								<Text ml="1rem">Upcoming</Text>
+							</MenuItem>
+							<MenuItem
+								onClick={(e) => {
+									handleClick("completed");
+								}}
+								className={activeTab === "completed" ? "active" : ""}
+							>
+								<BsCheckCircle fontSize={"1.4rem"} color="green" />
+								<Text ml="1rem">Completed</Text>
+							</MenuItem>
+							<MenuItem
+								onClick={(e) => {
+									handleClick("settings");
+								}}
+								className={activeTab === "settings" ? "active" : ""}
+							>
+								<AiOutlineSetting fontSize={"1.4rem"} color="#a8bfb8" />
+								<Text ml="1rem">Settings</Text>
+							</MenuItem>
+							<MenuItem
+								onClick={(e) => {
+									handleClick("login");
+								}}
+							>
+								<FiLogOut fontSize={"1.4rem"} color="#a8bfb8" />
+								<Text ml="1rem">Logout</Text>
+							</MenuItem>
 						</MenuList>
 					</Menu>
 				</div>
 			</div>
 		</div>
 	);
-
-              <MenuItem
-                mt="1rem"
-                onClick={(e) => {
-                  handleClick("today");
-                }}
-                className={activeTab === "today" ? "active" : ""}
-              >
-                <MdToday fontSize={"1.4rem"} color="purple" />
-                <Text ml="1rem">Today</Text>
-              </MenuItem>
-              <MenuItem
-                onClick={(e) => {
-                  handleClick("upcoming");
-                }}
-                className={activeTab === "upcoming" ? "active" : ""}
-              >
-                <FaRegCalendarAlt fontSize={"1.4rem"} color="brown" />
-                <Text ml="1rem">Upcoming</Text>
-              </MenuItem>
-              <MenuItem
-                onClick={(e) => {
-                  handleClick("completed");
-                }}
-                className={activeTab === "completed" ? "active" : ""}
-              >
-                <BsCheckCircle fontSize={"1.4rem"} color="green" />
-                <Text ml="1rem">Completed</Text>
-              </MenuItem>
-              <MenuItem
-                onClick={(e) => {
-                  handleClick("settings");
-                }}
-                className={activeTab === "settings" ? "active" : ""}
-              >
-                <AiOutlineSetting fontSize={"1.4rem"} color="#a8bfb8" />
-                <Text ml="1rem">Settings</Text>
-              </MenuItem>
-              <MenuItem
-                onClick={(e) => {
-                  handleClick("login");
-                }}
-              >
-                <FiLogOut fontSize={"1.4rem"} color="#a8bfb8" />
-                <Text ml="1rem">Logout</Text>
-              </MenuItem>
-            </MenuList>
-          </Menu>
-        </div>
-      </div>
-    </div>
-  );
-
 };
 
 export default Navbar;
