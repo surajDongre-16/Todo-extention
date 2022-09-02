4c73871baa200788d353dd0469502fd62f4e526a
surajDongre-16 <surajdongre8@gmail.com>
2022-09-02 17:59:54 +0530

    working on edit function

--- frontend/src/components/todoUi/Edit.jsx
+++ frontend/src/components/todoUi/Edit.jsx
@@ -1,83 +1,106 @@
-import  FocusLock from "react-focus-lock"
-import React  from "react";
-import {  EditIcon} from '@chakra-ui/icons'
+import FocusLock from "react-focus-lock";
+import React, { useState } from "react";
+import { EditIcon } from "@chakra-ui/icons";
 import {
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    FormControl,
-    FormLabel,
-    Input,
-    Button,
-    Stack,
-    ButtonGroup,
-    PopoverCloseButton,
-    IconButton,
-    PopoverArrow,
-    PopoverAnchor,
-    useDisclosure,
-  } from '@chakra-ui/react'
+  Popover,
+  PopoverTrigger,
+  PopoverContent,
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Stack,
+  ButtonGroup,
+  PopoverCloseButton,
+  IconButton,
+  PopoverArrow,
+  PopoverAnchor,
+  useDisclosure,
+} from "@chakra-ui/react";
+import axios from "axios";
 
 // 1. Create a text input component
-const TextInput = React.forwardRef(() => {
-    return (
+// const TextInput = React.forwardRef(() => {
+//     return (
+//       <FormControl>
+//         <FormLabel>Task</FormLabel>
+//         <Input  />
+//         <FormLabel>Description</FormLabel>
+//         <Input  />
+//       </FormControl>
+//     )
+//   })
+
+// 2. Create the form
+const Form = ({ onCancel,id }) => {
+
+  const [data, setFormData] = useState(null);
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setFormData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit=()=>{
+    // console.log(data)
+    for(let key in data){
+      if(!data[key]){
+        delete data[key]
+      }
+    }
+    console.log(data)
+    axios.put("http://localhost:5000/todo/user")
+  }
+
+  return (
+    <Stack spacing={4}>
       <FormControl>
         <FormLabel>Task</FormLabel>
-        <Input  />
+        <Input name="title" onChange={handleChange} />
         <FormLabel>Description</FormLabel>
-        <Input  />
+        <Input name="description" onChange={handleChange} />
       </FormControl>
-    )
-  })
-  
-  // 2. Create the form
-  const Form = ({ onCancel }) => {
-    return (
-      <Stack spacing={4}>
-      
-        <TextInput  />
-        <ButtonGroup display='flex' justifyContent='flex-end'>
-          <Button variant='outline' onClick={onCancel}>
-            Cancel
-          </Button>
-          <Button  colorScheme='red'>
-            Save
-          </Button>
-        </ButtonGroup>
-      </Stack>
-    )
-  }
-  
-  // 3. Create the Popover
-  // Ensure you set `closeOnBlur` prop to false so it doesn't close on outside click
-  const Edit = () => {
-    const { onOpen, onClose, isOpen } = useDisclosure()
-  
-  
-    return (
-      <>
-      
-        <Popover
-          isOpen={isOpen}
-        
-          onOpen={onOpen}
-          onClose={onClose}
-          placement='bottom'
-          closeOnBlur={false}
-        >
-          <PopoverTrigger>
-            <IconButton  mt="-2" size='sm' icon={<EditIcon />} />
-          </PopoverTrigger>
-          <PopoverContent p={5}>
-            <FocusLock returnFocus persistentFocus={false}>
-              <PopoverArrow />
-              <PopoverCloseButton />
-              <Form  onCancel={onClose} />
-            </FocusLock>
-          </PopoverContent>
-        </Popover>
-      </>
-    )
-  }
-  
- export default Edit
+      <ButtonGroup display="flex" justifyContent="flex-end">
+        <Button variant="outline" onClick={onCancel}>
+          Cancel
+        </Button>
+        <Button colorScheme="red" onClick={handleSubmit}>
+          Save
+        </Button>
+      </ButtonGroup>
+    </Stack>
+  );
+};
+
+// 3. Create the Popover
+// Ensure you set `closeOnBlur` prop to false so it doesn't close on outside click
+const Edit = ({id}) => {
+  const { onOpen, onClose, isOpen } = useDisclosure();
+
+  return (
+    <>
+      <Popover
+        isOpen={isOpen}
+        onOpen={onOpen}
+        onClose={onClose}
+        placement="bottom"
+        closeOnBlur={false}
+      >
+        <PopoverTrigger>
+          <IconButton mt="-2" size="sm" icon={<EditIcon />} />
+        </PopoverTrigger>
+        <PopoverContent p={5}>
+          <FocusLock returnFocus persistentFocus={false}>
+            <PopoverArrow />
+            <PopoverCloseButton />
+            <Form onCancel={onClose} id={id} />
+          </FocusLock>
+        </PopoverContent>
+      </Popover>
+    </>
+  );
+};
+
+export default Edit;
